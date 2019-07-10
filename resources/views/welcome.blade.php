ce7fa38905919cd1fab0bc920f47f28cf508b8c5
Nirav <nirav1549@gmail.com>
2019-07-11 00:09:13 +0530

    just testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    First project
                 </div>
 
                 <div class="links">
